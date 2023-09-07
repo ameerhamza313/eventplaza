0f8b7ad620f4f217ac7c3ac70341ceb45d14e639
Ameerhamza Sanadi <ameerhamzasanadi@gmail.com>
2023-09-07 11:08:48 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const path = require('path');
 
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 
 const EVENTS_FILE_PATH = 'events.json';
